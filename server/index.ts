16d7d91f0ce146521d0a25feaa1ae22a0668ad27
Smores25 <smores25@icloud.com>
2025-02-14 23:10:51 +0000

    idk

--- server/index.ts
+++ server/index.ts
@@ -58,8 +58,8 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
-  const PORT = 8380;
+  const PORT = 10000;
   server.listen(PORT, "0.0.0.0", () => {
-    log(`serving on port ${PORT}`);
+    log(`listening on port ${PORT}`);
   });
 })();
